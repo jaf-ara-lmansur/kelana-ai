259087ee5edd6b5f4156b9d8f4aac24aaeab9b2b
muhammad jafar almansur <muhammadjafaralmansur@MacBook-Air-muhammad.local>
2026-08-11 22:05:03 +0700

    create initial Kelana Ai console App

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+
+#def print_trip_summary(destination, days, budget, travel_style):
+   # print("==========")
+   # print("KelanaAI")
+   # print("==========")
+   # print(f"Destination     : {destination}")
+   # print(f"Days            : {days}")
+   # print(f"Budget          : {budget}")
+   # print(f"Style           : {travel_style}")
+
+# call any trip
+#print_trip_summary("Norway", 2, 1500, "Family")
+#print_trip_summary("Bone", 3, 400,"Solo-Mode")
+
+destination	=input("Destination : ")
+days		=int(input("Days : "))
+budget		=float(input("Budget : "))
+country		=input("Country : ")
+currency	=input("Currency : ")
+travel_month	=input("Travel_month : ")
+travel_style	=input("Travel_style : ")
